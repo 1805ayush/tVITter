759b5e95dabd34334ad8c6dd911ac209ab58d60b
1805ayush <ayush.chakaldar@gmail.com>
2022-11-24 21:30:03 +0530

    database connected

--- Tvitter-backend/index.js
+++ Tvitter-backend/index.js
@@ -1,5 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
+const mongoString = process.env.DATABASE_URL;
+
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error',(error)=>{
+    console.log(error);
+})
+
+database.once('connected',(error)=>{
+    console.log("Database connected");
+})
 
 const app  = express();
 

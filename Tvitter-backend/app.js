5cc249bba45f70a94d67cd02dc24dbad16e14321
1805ayush <ayush.chakaldar@gmail.com>
2022-11-28 17:58:15 +0530

    create post route and use middlewares

--- Tvitter-backend/app.js
+++ Tvitter-backend/app.js
@@ -3,4 +3,14 @@ const app = express();
 
 require('dotenv').config({path:"Tvitter-backend/config/config.env"})
 
+//using middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//importing routes
+const post = require("./routes/post");
+
+//using routes
+app.use("/api/v1",post);
+
 module.exports =app;
---
+++ Tvitter-backend/controllers/post.js
@@ -0,0 +1,30 @@
+const Post = require("../models/posts");
+
+exports.createPost = async(req,res)=>{
+
+    try {
+        
+        const newPostData = {
+            caption : req.body.caption,
+            image:{
+                publicID:"req.body.publicId",
+                url: "req.body.url"
+            },
+            user : req.user._id,
+        }
+
+        const newPost = await Post.create(newPostData);
+
+        res.status(200).json({
+            success:true,
+            post: newPost,
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message,
+        })
+    }
+
+};

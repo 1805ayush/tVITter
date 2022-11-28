b74048ff41736f39fb3f5fb4bb82574b37813709
1805ayush <ayush.chakaldar@gmail.com>
2022-11-28 16:33:09 +0530

    user and posts models

--- Tvitter-backend/models/users.js
+++ Tvitter-backend/models/users.js
@@ -2,26 +2,55 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
+    avatar:{
+        publicID: String,
+        url: String
+    },
     username:{
         type: String,
-        required: true,
+        required: [true,"Please enter a username"],
+        unique:[true,"Username already exists"]
     },
     name:{
         type: String,
-        required: true,
+        required: [true,"Please enter a name"],
     },
     email:{
         type: String,
-        required: true,
+        required: [true,"Please enter an email"],
+        unique:[true,"Email already exists"]
     },
-    password:{
+    password: {
         type: String,
-        required: true,
+        required: [true, "Please enter a password"],
+        minlength: [6, "Password must be at least 6 characters"],
+        select: false,
     },
     dob:{
         type: Date,
         default: Date.now,
-    }
+    },
+    posts:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        }
+    ],
+    followers:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    following:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
+
+
+
 });
 
-module.exports = User = mongoose.model("users",UserSchema);
+module.exports = User = mongoose.model("User",UserSchema);

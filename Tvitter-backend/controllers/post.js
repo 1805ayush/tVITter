b0667b7e746fa65594bcf40793dbe90486f46448
1805ayush <ayush.chakaldar@gmail.com>
2022-11-30 20:10:38 +0530

    Like post route

--- Tvitter-backend/controllers/post.js
+++ Tvitter-backend/controllers/post.js
@@ -19,9 +19,6 @@ exports.createPost = async (req,res)=>{
 
         const user = await User.findById(req.user._id);
 
-        console.log(newPost._id);
-        user.posts.push(newPost._id);
-
         await user.save();
 
         res.status(201).json({
@@ -37,3 +34,44 @@ exports.createPost = async (req,res)=>{
     }
 
 };
+
+exports.likeAndUnlikePost = async (req, res)=>{
+    try {
+        
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            return res.status(404).json({
+                success: false,
+                message: true,
+            })
+        }
+
+        if (post.likes.includes(req.user._id)) {
+            const index = await post.likes.indexOf(req.user._id);
+            post.likes.splice(index,1);
+
+            await post.save();
+            return res.status(200).json({
+                success: true,
+                message: "Post Unliked"
+            })
+        }else{
+            post.likes.push(req.user._id);
+
+            await post.save();
+            return res.status(200).json({
+                success: true,
+                message: "Post Liked"
+            })
+        }
+
+    } catch (error) {
+        
+        res.status(500).json({
+            success: false,
+            message: error.message
+        })
+
+    }
+}

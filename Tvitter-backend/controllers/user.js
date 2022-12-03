4aa8bc33e1bd21d0442c1a62482780e92559e715
1805ayush <ayush.chakaldar@gmail.com>
2022-12-03 18:22:55 +0530

    Update delete route

--- Tvitter-backend/controllers/user.js
+++ Tvitter-backend/controllers/user.js
@@ -208,11 +208,32 @@ exports.deleteProfile = async (req,res)=>{
         
         const user = await User.findById(req.user._id);
         const posts = user.posts;
+        const followers = user.followers;
+        const followings = user.following;
+        const userId = user._id;
         await user.remove();
 
         for(let i = 0;i<posts.length;i++){
             const post = await Post.findById(posts[i]);
             await post.remove();
+        }
+
+        for(let i = 0;i<followers.length;i++){
+            const follower = await User.findById(followers[i]);
+
+            const index = await follower.followers.indexOf(follower);
+            follower.followers.splice(index,1);
+
+            await follower.save();
+        }
+
+        for(let i = 0;i<followings.length;i++){
+            const following = await User.findById(followings[i]);
+
+            const index = await following.following.indexOf(following);
+            following.following.splice(index,1);
+
+            await following.save();
         }
 
         return res.status(200)
